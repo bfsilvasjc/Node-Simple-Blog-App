2025ae614d2f242661230e7a8e9e3a26be3e3cfd
Bruno <bfcarvalho2002@gmail.com>
2021-03-02 00:01:11 -0300

    Implementação da função de edição das categorias

--- blogapp/routes/admin.js
+++ blogapp/routes/admin.js
@@ -5,7 +5,7 @@ require('../models/Categoria')
 const Categoria = mongoose.model('categorias')
 
 router.get('/', (req, res)=>{
-    res.render('admin/index')
+    res.redirect('/admin/categorias')
 })
 
 router.get('/posts', (req, res)=>{
@@ -59,6 +59,34 @@ router.post('/categorias/nova', (req, res)=>{
             res.redirect('/admin')
         })
     }
+})
+
+router.get('/categorias/edit/:id', (req, res)=>{
+    Categoria.findOne({_id:req.params.id}).then((categoria)=>{
+        res.render('admin/editcategorias', categoria)
+    }).catch((err)=>{
+        console.log('Ocorreu um erro: ' + err)
+        req.flash('error_msg', 'Esta categoria não existe!')
+        res.redirect('/admin/categorias')
+    })
+})
+
+router.post('/categorias/edit', (req, res)=>{
+    Categoria.findOne({_id: req.body.id}).then((categoria)=>{
+        categoria.nome = req.body.nome
+        categoria.slug = req.body.slug
+        categoria.save().then(()=>{
+            req.flash('success_msg', 'Categoria editada com sucesso!')
+            res.redirect('/admin/categorias')
+        }).catch((err)=>{
+            console.log('Ocorreu um erro: ' + err)
+            req.flash('error_msg', 'Houve um erro interno ao editar a categoria!')
+            res.redirect('/admin/categorias')
+        })
+    }).catch((err)=>{
+        req.flash('error_msg', 'Houve um erro ao editar a categoria!')
+        res.redirect('/admin/categorias')
+    })
 })
 
 module.exports = router;
