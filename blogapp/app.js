9b96f5f7ced3ffb7c7ff18608c73dd7d418eb565
Bruno <bfcarvalho2002@gmail.com>
2021-03-13 17:15:28 -0300

    Adição da funcionalidade de home page do sistema, área publica

--- blogapp/app.js
+++ blogapp/app.js
@@ -8,6 +8,10 @@ const path = require('path')
 const mongoose = require('mongoose')
 const session = require('express-session')
 const flash = require('connect-flash')
+require('./models/Postagem')
+const Postagem = mongoose.model('postagens')
+require('./models/Categoria')
+const Categoria = mongoose.model('categorias')
 
 // Configurações
 
@@ -46,11 +50,65 @@ mongoose.connect('mongodb://localhost/blogapp').then(()=>{
 // PUBLIC
 app.use(express.static(path.join(__dirname, 'public')))
 app.use((req, res, next)=>{
-    console.log('aqui é um middleware!')
     next()
 })
 
 // Rotas
+app.get('/', (req, res)=>{
+    Postagem.find().lean().populate('categoria').sort({data:'desc'}).then((postagens)=>{
+        res.render('index', {postagens})
+    }).catch((err)=>{
+        req.flash('error_msg', 'Houve um erro ao listar as categorias')
+        res.redirect('/404')
+    })
+})
+
+app.get('/404', (req, res)=>{
+    res.send('Erro 404!')
+})
+
+app.get('/postagem/:slug', (req, res)=>{
+    Postagem.findOne({slug: req.params.slug}).then((postagem)=>{
+        if(postagem){
+            res.render('postagem/index', postagem)
+        }else{
+            req.flash('error_msg', 'Essa postagem não existe!')
+            res.redirect('/')
+        }
+    }).catch((err)=>{
+        req.flash('error_msg', 'Houve um erro interno!')
+        res.redirect('/')
+    })
+})
+
+app.get('/categorias', (req, res)=>{
+    Categoria.find().lean().sort({date:'desc'}).then((categorias)=>{
+        res.render('./categorias/index', {categorias})
+    }).catch((err)=>{
+        req.flash('error_msg', 'Houve um erro ao listar as categorias')
+        res.redirect('/')
+    })
+})
+
+app.get('/categorias/:slug', (req, res)=>{
+    Categoria.findOne({slug:req.params.slug}).then((categoria)=>{
+        if(categoria){
+            Postagem.find({categoria: categoria._id}).lean().then((postagens)=>{
+                res.render('categorias/postagens', {categoria, postagens})
+            }).catch((err)=>{
+                req.flash('error_msg', 'Houve um erro interno ao listar os posts!')
+                res.redirect('/')
+            })
+        }else{
+            req.flash('error_msg', 'Essa categoria não existe!')
+            res.redirect('/')
+        }
+    }).catch((err)=>{
+        req.flash('error_msg', 'Houve um erro interno!')
+        res.redirect('/')
+    })
+})
+
 app.use('/admin', admin)
 
 // Outros
